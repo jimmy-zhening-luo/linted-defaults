e2ca29ef20f279eb919da1965ecc6cf98a932a45
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-12-01 12:19:47 -0800

    rev eslint 9.16: disable more rules handled by tsc; +1 redundancy from disable-ext
    - getter-return
    - no-class-assign
    - no-import-assign
    - no-new-native-constructor
    - (redundant) no-invalid-this

--- src/rules/ts/disable.ts
+++ src/rules/ts/disable.ts
@@ -8,11 +8,16 @@ export const TsDisable = [
   {
     // Handled by TypeScript compiler
     "constructor-super": OFF,
+    "getter-return": OFF,
+    "no-class-assign": OFF,
     "no-const-assign": OFF,
     "no-dupe-args": OFF,
     "no-dupe-class-members": OFF,
     "no-dupe-keys": OFF,
     "no-func-assign": OFF,
+    "no-import-assign": OFF,
+    "no-invalid-this": OFF,
+    "no-new-native-nonconstructor": OFF,
     "no-obj-calls": OFF,
     "no-setter-return": OFF,
     "no-this-before-super": OFF,
