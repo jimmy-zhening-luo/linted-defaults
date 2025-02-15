ebfe016f37075394159de14812cb32416a179dde
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2025-02-14 16:37:52 -0800

    rules(css): support css .3
    - +css/require-baseline{available}

--- src/rules/css/enable.ts
+++ src/rules/css/enable.ts
@@ -12,6 +12,12 @@ export const CssEnable = [
     "css/no-empty-blocks": ERROR,
     "css/no-invalid-at-rules": ERROR,
     "css/no-invalid-properties": ERROR,
+    "css/require-baseline": [
+      ERROR,
+      {
+        available: "widely" /* "newly" | @default "widely" */,
+      },
+    ],
     "css/use-layers": OFF /* not recommended by docs; also BUG: ESLint config inspector: css/use-layers: Invalid rule has no description [INVALID] */,
   } as const,
 ] as const;
