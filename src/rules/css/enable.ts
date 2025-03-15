8bc6e3e3e5355bbe5395a0ffb1ef19e7c7a54489
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-03-14 18:05:42 -0700

    csslint 0.5
    + prefer-logical-properties
    + add link to use-layers bug
    + replace doc with actual doc

--- src/rules/css/enable.ts
+++ src/rules/css/enable.ts
@@ -7,17 +7,24 @@ import {
 export const CssEnable = {
   id: ID.Enable,
   rules: {
-  // DOC: https://github.com/eslint/css?tab=readme-ov-file#rules
+    // DOC: https://github.com/eslint/css/tree/main/docs/rules
     "css/no-duplicate-imports": ERROR,
     "css/no-empty-blocks": ERROR,
     "css/no-invalid-at-rules": ERROR,
     "css/no-invalid-properties": ERROR,
+    "css/prefer-logical-properties": [
+      ERROR,
+      {
+        allowProperties: [],
+        allowUnits: [],
+      },
+    ],
     "css/require-baseline": [
       ERROR,
       {
-        available: "widely" /* "newly" | @default "widely" */,
+        available: "widely" /* newly | @default widely */,
       },
     ],
-    "css/use-layers": OFF /* not recommended by docs; also BUG: ESLint config inspector: css/use-layers: Invalid rule has no description [INVALID] */,
+    "css/use-layers": OFF, /* not recommended by docs: https://github.com/eslint/css?tab=readme-ov-file#rules; also BUG: ESLint config inspector: css/use-layers: Invalid rule has no description [INVALID] */
   } as const,
 } as const;
