ef5ca812da3b82c8ae478379919d79b5ed9bb5ac
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-06-11 16:18:31 -0700

    eslint-plugin-chai-friendly: v1.1.0; looks like these guys got the hint too

--- src/rules/scopes/mocha/disable.ts
+++ src/rules/scopes/mocha/disable.ts
@@ -7,7 +7,7 @@ export const MochaDisable = {
   id: ID.Disable.Disable,
   rules: {
     "prefer-arrow-callback": OFF /* TS-capable */,
-    "no-unused-expressions": OFF,
+    "no-unused-expressions": OFF /* INFO: looks like these guys got the hint too ;) https://github.com/ihordiachenko/eslint-plugin-chai-friendly/releases/tag/v1.1.0 */,
     // "@typescript-eslint/no-unused-expressions": OFF /* ESLint now supports TS */,
   } as const,
 };
