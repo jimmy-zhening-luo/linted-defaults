ebcb6a55f91f5ac2e90a4f52821392b9dc677e8b
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-06-28 14:30:02 -0700

    most jsonc should allow comma though

--- src/files/scopes/jsoncc.ts
+++ src/files/scopes/jsoncc.ts
@@ -1,7 +1,7 @@
 import { CreateScope } from "../utility";
 
 export default CreateScope(
-  "code-snippets",
+  "{jsonc,code-snippets}",
   {
     filenames: ["{tsconfig,.vscode/**/*}.json"],
     subdirectories: [".vscode"],
