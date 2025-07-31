e0c0076bdde723e0160c1d6383d6b5d63343d5cf
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-07-30 18:51:21 -0700

    compress

--- src/defaults/index.ts
+++
@@ -1,9 +0,0 @@
-import { files } from "./files";
-import { ignores } from "./ignores";
-import { rules } from "./rules";
-
-export const defaults = {
-  files,
-  ignores,
-  rules,
-};
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
+import { files } from "./files";
+import { ignores } from "./ignores";
+import { rules } from "./rules";
+
 export { settings } from "./settings";
-export { defaults } from "./defaults";
+export const defaults = {
+  files,
+  ignores,
+  rules,
+};
