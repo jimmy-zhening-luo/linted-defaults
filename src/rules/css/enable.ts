fbb08c886ae1400462e20ad3dda34e125e8b2ec3
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-09-03 11:27:43 -0700

    css .11, and attempt to re-enable no-invalid-properties, -% from allowed font units

--- src/rules/css/enable.ts
+++ src/rules/css/enable.ts
@@ -8,23 +8,17 @@ export const CssEnable = {
   id: ID.Enable.Enable,
   rules: {
     // DOC: https://github.com/eslint/css/tree/main/docs/rules
+    "css/font-family-fallbacks": ON,
     "css/no-duplicate-imports": ON,
+    "css/no-duplicate-keyframe-selectors": ON,
     "css/no-empty-blocks": ON,
     "css/no-important": ON,
     "css/no-invalid-at-rule-placement": ON,
     "css/no-invalid-at-rules": ON,
     "css/no-invalid-named-grid-areas": ON,
-    // "css/no-invalid-properties": OFF /* BUG: disabled with PREJUDICE (see commit: https://github.com/jimmy-zhening-luo/linted-defaults/commit/702e6632328813e84df92cade730469661ee85db) */,
+    "css/no-invalid-properties": ON,
     "css/prefer-logical-properties": ON,
-    "css/relative-font-units": [
-      ON,
-      {
-        allowUnits: [
-          "rem",
-          "%",
-        ],
-      },
-    ],
+    "css/relative-font-units": ON,
     "css/use-baseline": ON,
     // "css/use-layers": OFF, /* not recommended by docs: https://github.com/eslint/css?tab=readme-ov-file#rules; also BUG: ESLint config inspector: css/use-layers: Invalid rule has no description [INVALID] */
   } as const,
