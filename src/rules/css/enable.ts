de65ed721c177e2fd351ec3ebe156c74d37277c0
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-10-10 19:17:54 -0700

    css .13

--- src/rules/css/enable.ts
+++ src/rules/css/enable.ts
@@ -19,6 +19,7 @@ export const CssEnable = {
     // "css/no-invalid-properties": OFF /* BUG: this shit is still way too broken, it doens't recognize actual rules. in futue if it recognizes actual rules, turn on allow unknown values btw */,
     "css/prefer-logical-properties": ON,
     "css/relative-font-units": ON,
+    // "css/selector-complexity": OFF /* preference, or rather too lazy to configure because its default state is literally infinitely permissable */,
     "css/use-baseline": ON,
     // "css/use-layers": OFF, /* not recommended by docs: https://github.com/eslint/css?tab=readme-ov-file#rules; also BUG: ESLint config inspector: css/use-layers: Invalid rule has no description [INVALID] */
   } as const,
