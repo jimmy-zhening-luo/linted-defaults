e1d57e6c418039c400fb4624716068a54a5781f0
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-10-17 22:52:03 -0700

    refactor: simplify global ignore patterns and remove unused variables

--- src/ignores/global.ts
+++ src/ignores/global.ts
@@ -1,36 +1,19 @@
-import {
-  ROOTS,
-  SUBROOTS,
-} from "../_pattern";
-
-const ext = [
-  "**/*.js",
-  "**/*.cjs",
-  "**/*.mjs",
-],
-subroots = SUBROOTS.filter(subroot => subroot !== ""),
-folders = ext.flatMap(
-  ext => subroots.map(
-    subroot => subroot.concat(
-      "/",
-      ext,
-    ),
-  ),
-);
+import { pattern } from "../_pattern";
 
 export default [
   "**/.git/",
-  ...ext,
+  "**/*.js",
+  "**/*.cjs",
+  "**/*.mjs",
   "!*.js",
   "!*.cjs",
   "!*.mjs",
 ]
   .concat(
-    ROOTS
-      .flatMap(
-        root => folders.map(
-          folder => root.concat(folder),
-        ),
-      )
+    pattern([
+      "*.js",
+      "*.cjs",
+      "*.mjs",
+    ])
       .map(path => "!".concat(path)),
   );
