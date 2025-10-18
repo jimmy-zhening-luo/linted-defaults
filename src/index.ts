0087be327604deda600914d8e7becc1e8da302ec
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-10-17 18:38:25 -0700

    migrate settings to linted

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import settings from "./settings";
 import files from "./files";
 import ignores from "./ignores";
 import rules from "./rules";
 
 export default {
-  settings,
   files,
   ignores,
   rules,
--- src/settings/yml.ts
+++
@@ -1,7 +0,0 @@
-// DOC: https://github.com/ota-meshi/yaml-eslint-parser?tab=readme-ov-file#advanced-configuration
-export default {
-  plugins: ["yml"] as const,
-  languageOptions: {
-    parser: "yml" as const,
-  },
-};
