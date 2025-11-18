49fc1656baee97492b581d036fb5ae8dd3924b87
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-11-17 18:08:39 -0800

    Add rule for unused private class members in TS config

--- src/rules/ts/ts.ts
+++ src/rules/ts/ts.ts
@@ -116,6 +116,7 @@ export default {
     "ts/no-unsafe-return": State.ON,
     // "ts/no-unsafe-type-assertion": State.OFF,
     "ts/no-unsafe-unary-minus": State.ON,
+    "ts/no-unused-private-class-members": State.ON,
     "ts/no-useless-empty-export": State.ON,
     "ts/no-wrapper-object-types": State.ON,
     // "ts/non-nullable-type-assertion-style": State.OFF /* preference */,
