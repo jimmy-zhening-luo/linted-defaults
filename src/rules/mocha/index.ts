da5623dafda500cee1de85908eb9bf7595a67881
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-11-24 04:00:15 -0800

    Fix rule name in mocha index file

--- src/rules/mocha/index.ts
+++ src/rules/mocha/index.ts
@@ -3,7 +3,7 @@ import { State } from "../state";
 export default [
   {
     rules: {
-      "ts/no-unused-expressions": State.OFF /* fails to detect valueful assertions in mocha */,
+      "no-unused-expressions": State.OFF /* fails to detect valueful assertions in mocha */,
     } as const,
   },
 ];
