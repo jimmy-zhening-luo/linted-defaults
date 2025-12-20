1c1bb4cf1946584bcd921e64b465235fa0f0d155
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-12-20 12:58:15 -0800

    fix mocha rule name

--- src/rules/mocha/index.ts
+++ src/rules/mocha/index.ts
@@ -3,8 +3,8 @@ import { State } from "../state";
 export default [
   {
     rules: {
-      "no-unsafe-call": State.OFF /* fails to detect valueful assertions in mocha */,
       "no-unused-expressions": State.OFF /* fails to detect valueful assertions in mocha */,
+      "ts/no-unsafe-call": State.OFF /* fails to detect valueful assertions in mocha */,
     } as const,
   },
 ];
