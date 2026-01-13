23af9b62b3c9de506fe8694a7bc9a36d51b04e55
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-01-12 18:15:39 -0800

    Add strict-void-return rule to mocha configuration

--- src/rules/mocha/index.ts
+++ src/rules/mocha/index.ts
@@ -5,6 +5,7 @@ export default [
     rules: {
       "no-unused-expressions": State.OFF /* fails to detect valueful assertions in mocha */,
       "ts/no-unsafe-call": State.OFF /* fails to detect valueful assertions in mocha */,
+      "ts/strict-void-return": State.OFF /* false positive in mocha test-case async function context */,
     } as const,
   },
 ];
