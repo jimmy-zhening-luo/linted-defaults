a42b8c1237b9bb113f987774e211c7bb64dffaaa
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-02-12 19:31:22 -0800

    Remove unused jsoncExtend and related plugin file

--- src/rules/json/index.ts
+++ src/rules/json/index.ts
@@ -1,9 +1,7 @@
 import json from "./json";
 import jsonc from "./plugin";
-import jsoncExtend from "./plugin/extend";
 
 export default [
   json,
   jsonc,
-  jsoncExtend,
 ];
--- src/rules/json/plugin/index.ts
+++ src/rules/json/plugin/index.ts
@@ -1,6 +1,6 @@
 import { State } from "../../state";
 
-// DOC: https://ota-meshi.github.io/eslint-plugin-jsonc/rules/#jsonc-rules
+// DOC: https://ota-meshi.github.io/eslint-plugin-jsonc/rules/
 export default {
   rules: {
     // "jsonc/auto": State.OFF,
@@ -26,5 +26,48 @@ export default {
     // "jsonc/sort-keys": State.OFF /* ESLint: json/sort-keys */,
     // "jsonc/valid-json-number": State.OFF,
     // "jsonc/vue-custom-block/no-parsing-error": State.OFF,
+    // "jsonc/array-bracket-newline": State.OFF /* BUG: DO NOT CONFIGURE */,
+    "jsonc/array-bracket-spacing": State.WARN,
+    "jsonc/array-element-newline": [
+      State.WARN,
+      "consistent",
+    ],
+    "jsonc/comma-dangle": State.ON,
+    "jsonc/comma-style": State.WARN,
+    "jsonc/indent": [
+      State.WARN,
+      2,
+    ],
+    "jsonc/key-spacing": State.WARN,
+    // "jsonc/no-dupe-keys": State.OFF /* ESLint: json/no-duplicate-keys */,
+    // "jsonc/no-floating-decimal": State.OFF,
+    "jsonc/no-irregular-whitespace": [
+      State.ON,
+      {
+        skipStrings: true,
+        skipComments: true,
+      },
+    ],
+    // "jsonc/no-multi-str": State.OFF,
+    // "jsonc/no-octal-escape": State.OFF,
+    // "jsonc/no-octal": State.OFF,
+    // "jsonc/no-sparse-arrays": State.OFF,
+    // "jsonc/no-useless-escape": State.OFF,
+    "jsonc/object-curly-newline": [
+      State.WARN,
+      {
+        consistent: true,
+        multiline: true,
+        minProperties: 2,
+      },
+    ],
+    "jsonc/object-curly-spacing": [
+      State.WARN,
+      "always",
+    ],
+    "jsonc/object-property-newline": State.WARN,
+    // "jsonc/quote-props": State.OFF,
+    // "jsonc/quotes": State.OFF,
+    // "jsonc/space-unary-ops": State.OFF,
   } as const,
 };
