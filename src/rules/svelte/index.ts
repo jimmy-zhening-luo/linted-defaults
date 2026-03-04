468baed8e0c2ffd3058972a9550613209b1d6e5e
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-03-04 15:40:57 -0800

    Refactor index.ts to maintain commented-out html import

--- src/rules/svelte/index.ts
+++ src/rules/svelte/index.ts
@@ -1,9 +1,11 @@
 import disable from "./disable";
 import svelte from "./svelte";
 import extend from "./extend";
+// import html from "./html";
 
 export default [
   disable,
   svelte,
   extend,
+  // html,
 ];
