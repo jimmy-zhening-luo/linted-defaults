535230c56e1f50706240676024b826eace6d7bc7
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-03-06 16:55:25 -0800

    Add exp-list-style rule with maxItems configurations

--- src/rules/global.ts
+++ src/rules/global.ts
@@ -5,6 +5,51 @@ export default [
     rules: {
       "stylistic/eol-last": State.WARN,
       "stylistic/linebreak-style": State.WARN,
+      "stylistic/exp-list-style": [
+        State.OFF,
+        {
+          singleLine: {
+            maxItems: 2,
+          },
+          overrides: {
+            ExportNamedDeclaration: {
+              singleLine: {
+                maxItems: 1,
+              },
+            },
+            TSInterfaceBody: {
+              singleLine: {
+                maxItems: 0,
+              },
+            },
+            TSEnumBody: {
+              singleLine: {
+                maxItems: 0,
+              },
+            },
+            TSTupleType: {
+              singleLine: {
+                maxItems: 3,
+              },
+            },
+            TSTypeParameterDeclaration: {
+              singleLine: {
+                maxItems: 1,
+              },
+            },
+            JSONArrayExpression: {
+              singleLine: {
+                maxItems: 0,
+              },
+            },
+            JSONObjectExpression: {
+              singleLine: {
+                maxItems: 0,
+              },
+            },
+          },
+        },
+      ],
     } as const,
   },
 ];
